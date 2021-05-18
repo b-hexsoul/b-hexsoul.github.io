6c4ba0ca4f2e746ed94386b484158b2c69d1efb8
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-05-18 10:26:44 -0700

    Updates

---
+++ precache-manifest.ecd9fbc9f3e835b14195f6ca1120cfd5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b6d4f88a7f7a6d247aaab55999b76a",
+    "url": "/brandon.is-a.dev/index.html"
+  },
+  {
+    "revision": "b17ffcb20f87a10c2e89",
+    "url": "/brandon.is-a.dev/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "b7cfb717e639a747108a",
+    "url": "/brandon.is-a.dev/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "792389926d8f4a687f4a",
+    "url": "/brandon.is-a.dev/static/css/main.7304f5f0.chunk.css"
+  },
+  {
+    "revision": "3cbd47fea3f31e4d580d",
+    "url": "/brandon.is-a.dev/static/js/2.28b3fc86.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/brandon.is-a.dev/static/js/2.28b3fc86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17ffcb20f87a10c2e89",
+    "url": "/brandon.is-a.dev/static/js/3.3d3fc74b.chunk.js"
+  },
+  {
+    "revision": "b7cfb717e639a747108a",
+    "url": "/brandon.is-a.dev/static/js/4.e2749876.chunk.js"
+  },
+  {
+    "revision": "792389926d8f4a687f4a",
+    "url": "/brandon.is-a.dev/static/js/main.a2f1370e.chunk.js"
+  },
+  {
+    "revision": "b22f94e97906fb707128",
+    "url": "/brandon.is-a.dev/static/js/runtime-main.d4a6a1d4.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/brandon.is-a.dev/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/brandon.is-a.dev/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c7401f520e568a2994f0269e6c537f1d",
+    "url": "/brandon.is-a.dev/static/media/aws-logo.c7401f52.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/brandon.is-a.dev/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/brandon.is-a.dev/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c7aaa78390000537552e08d8d737345e",
+    "url": "/brandon.is-a.dev/static/media/expedia-logo.c7aaa783.png"
+  },
+  {
+    "revision": "bed2081b3216b2df6653ac3d55d900c4",
+    "url": "/brandon.is-a.dev/static/media/hudello-logo.bed2081b.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/brandon.is-a.dev/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "5e5b4d17cb4fc29e3c405f6cc5218ee1",
+    "url": "/brandon.is-a.dev/static/media/mitx.5e5b4d17.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/brandon.is-a.dev/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/brandon.is-a.dev/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "d4537055ea001e9b7d426bf5d45321ea",
+    "url": "/brandon.is-a.dev/static/media/su-logo.d4537055.png"
+  },
+  {
+    "revision": "f0614fc19571c472f6c4987978bd6047",
+    "url": "/brandon.is-a.dev/static/media/uw-logo.f0614fc1.png"
+  }
+]);
